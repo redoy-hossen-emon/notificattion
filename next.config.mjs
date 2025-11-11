b193456a905820037e1a68ea5bff1b892dbf71d5
redoy hossen <redoy.hossen.emon@mail.com>
2025-11-11 07:21:18 +0600

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export", // Required for static export (Capacitor will load files from /out)
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Prevents image optimization errors in static export
+  },
+  trailingSlash: true, // Optional: ensures proper routing for static exports
 };
 
 export default nextConfig;
